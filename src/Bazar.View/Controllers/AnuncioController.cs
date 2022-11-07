4664601a60cb71a1a1bc85f804b54f26213bc763
JacksonSM <jacksons.m2018@gmail.com>
2022-11-07 13:09:33 -0300

    Feito fomulario para criação de anuncio.

--- src/Bazar.View/Controllers/AnuncioController.cs
+++ src/Bazar.View/Controllers/AnuncioController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bazar.View.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bazar.View.Controllers;
 public class AnuncioController : Controller
 {
+    [HttpGet]
     public IActionResult CriarAnuncio()
     {
         return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public Task<IActionResult> CriarAnuncio(IFormFile imagemPrincipal, IFormFileCollection imagensSegundaria,
+    AnuncioViewModel anuncioVM)
+    {
+        throw new NotImplementedException();
     }
 }
--- src/Bazar.View/Controllers/HomeController.cs
+++ src/Bazar.View/Controllers/HomeController.cs
@@ -5,14 +5,15 @@ using System.Diagnostics;
 namespace Bazar.View.Controllers;
 public class HomeController : Controller
 {
+    private readonly ILogger<HomeController> _logger;
 
-    public HomeController()
+    public HomeController(ILogger<HomeController> logger)
     {
+        _logger = logger;
     }
 
     public IActionResult Index()
     {
-
         return View(new List<AnuncioViewModel>());
     }
 
@@ -20,7 +21,6 @@ public class HomeController : Controller
     {
         return View();
     }
-
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
