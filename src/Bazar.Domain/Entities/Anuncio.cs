356167258007a40eacb776eb116a7e0752a275c7
JacksonSM <jacksons.m2018@gmail.com>
2022-11-08 08:24:04 -0300

    Feito validação para anuncio.

--- src/Bazar.Domain/Entities/Anuncio.cs
+++ src/Bazar.Domain/Entities/Anuncio.cs
@@ -1,4 +1,6 @@
-﻿namespace Bazar.Domain.Entities;
+﻿using Bazar.Domain.Validation;
+
+namespace Bazar.Domain.Entities;
 public class Anuncio : BaseEntity
 {
     public string Titulo { get; private set; }
@@ -13,14 +15,32 @@ public class Anuncio : BaseEntity
 
     public Anuncio(){}//Para EF
 
-    public Anuncio(string titulo, string descricao, int tempoUso,
+    public Anuncio(string titulo, string descricao, int tempoUso, string cidade, decimal preco)
+    {
+        Validar(titulo, descricao, tempoUso, cidade ,preco);
+        Ativo = true;
+    }
+
+    public void Validar(string titulo, string descricao, int tempoUso,
         string cidade, decimal preco)
     {
+        DomainExceptionValidation.Quando(string.IsNullOrEmpty(titulo),
+        "O campo Titulo é obrigatório");
+
+        DomainExceptionValidation.Quando(string.IsNullOrEmpty(descricao),
+        "O campo Descrição é obrigatório");
+
+        DomainExceptionValidation.Quando(string.IsNullOrEmpty(cidade),
+        "O campo Cidade é obrigatório");
+
+        DomainExceptionValidation.Quando(preco < 0, "Preço não pode ser negativo.");
+
+        DomainExceptionValidation.Quando(tempoUso < 0, "Tempo de uso não pode ser negativo.");
+
         Titulo = titulo;
         Descricao = descricao;
         TempoUso = tempoUso;
         Cidade = cidade;
         Preco = preco;
-        Ativo = true;
     }
 }
---
+++ src/Bazar.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,13 @@
+﻿namespace Bazar.Domain.Validation;
+
+public class DomainExceptionValidation : Exception
+{
+    public DomainExceptionValidation(string error) : base(error)
+    { }
+
+    public static void Quando(bool temErro, string mensagemErro)
+    {
+        if (temErro)
+            throw new DomainExceptionValidation(mensagemErro);
+    }
+}
