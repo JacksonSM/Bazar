c31f65b5843b13f940f7519a5ed8ca7cf60695fa
JacksonSM <jacksons.m2018@gmail.com>
2022-11-08 11:13:38 -0300

    Ferramentas para salvar imagem.

---
+++ src/Bazar.View/Tools/Imagens/GerenciadorImagens.cs
@@ -0,0 +1,65 @@
+﻿using Bazar.View.Tools.Upload;
+
+namespace Bazar.View.Tools.Imagens;
+
+public class GerenciadorImagens
+{
+    private readonly string[] EXTENSAO_PERMITIDOS = { ".jpeg" };
+    private readonly UnitOfUpload _upload;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    public GerenciadorImagens(UnitOfUpload upload, IWebHostEnvironment webHostEnvironment)
+    {
+        _upload = upload;
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    public void ExcluirImagem(string nomeImagem)
+    {
+        var path = Path.Combine(_webHostEnvironment.WebRootPath + $"\\uploads\\imagens_produtos\\{nomeImagem}");
+        File.Delete(path);
+    }
+
+    public void ExcluirImagem(string[] listaNomes)
+    {
+        foreach (var nomeImagem in listaNomes)
+        {
+            ExcluirImagem(nomeImagem);
+        }
+    }
+
+    public string SalvarImagem(IFormFile imagem)
+    {
+        Validar(imagem.FileName);
+        var nomeImagem = Guid.NewGuid().ToString();
+        _upload.CarregarImagem(imagem, nomeImagem);
+        return nomeImagem + Path.GetExtension(imagem.FileName);
+    }
+
+    public List<string> SalvarImagem(IFormFileCollection imagens)
+    {
+        Validar(imagens.Select(c => c.FileName).ToArray());
+
+        var nomesImagens = new List<string>();
+
+        foreach (var imagem in imagens)
+        {
+            nomesImagens.Add(SalvarImagem(imagem));
+        }
+        return nomesImagens;
+    }
+
+    private void Validar(string fileName)
+    {
+        if (!EXTENSAO_PERMITIDOS.Contains(Path.GetExtension(fileName)))
+                throw new FormatException("Apenas formatos jpeg são permitidos.");
+    }
+
+    private void Validar(string[] fileNames)
+    {
+        foreach (var fileName in fileNames)
+        {
+            Validar(fileName);
+        }
+    }
+
+}
---
+++ src/Bazar.View/Tools/Upload/UnitOfUpload.cs
@@ -0,0 +1,51 @@
+﻿namespace Bazar.View.Tools.Upload;
+
+public class UnitOfUpload
+{
+
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public UnitOfUpload(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    public async void CarregarImagem(IFormFile file, string codigo)
+    {
+        await CarregarImagem(file, codigo, "\\uploads\\imagens_produtos\\");
+    }
+
+    public async Task CarregarImagem(IFormFile file, string nomeImagem, string path)
+    {
+        long totalBytes = file.Length;
+        string fileName = file.FileName.Trim('"');
+        var extension = Path.GetExtension(fileName);
+        fileName = nomeImagem + extension;
+
+
+        fileName = fileName.Contains("\\") ? fileName.Substring(fileName.LastIndexOf("\\") + 1) : fileName;
+
+        byte[] buffer = new byte[1024 * 1024];
+        using (FileStream output = File.Create(ObterCaminhoMaisNomeDoArquivo(path, fileName)))
+        {
+            using (Stream input = file.OpenReadStream())
+            {
+                int readBytes;
+                while ((readBytes = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    await output.WriteAsync(buffer, 0, readBytes);
+                    totalBytes += readBytes;
+                }
+            }
+        }
+    }
+
+    private string ObterCaminhoMaisNomeDoArquivo(string path, string fileName)
+    {
+        path = _webHostEnvironment.WebRootPath + path;
+        if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+        return path + fileName;
+    }
+}
+
